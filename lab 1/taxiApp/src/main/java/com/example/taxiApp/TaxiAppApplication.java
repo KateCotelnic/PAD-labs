1aca693c48d2d30d1ec0b74d9b2b7cee6c119c3a
Ecaterina Cotelnic [Intern] <Ecaterina.Cotelnic@endava.com>
2021-10-10 23:33:11 +0300

    create gateway

---
+++ lab 1/taxiApp/src/main/java/com/example/taxiApp/TaxiAppApplication.java
@@ -0,0 +1,37 @@
+package com.example.taxiApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class TaxiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiAppApplication.class, args);}
+
+		@Bean
+		RouteLocator gateway (RouteLocatorBuilder rlb){
+			return rlb
+					.routes()
+					.route("newTrip", routeSpec -> routeSpec
+							.path("/new")
+							.filters(gatewayFilterSpec ->
+									gatewayFilterSpec.setPath("/newTrip"))
+							.uri("http://localhost:9191/")
+					)
+					.route("trip", routeSpec -> routeSpec
+							.path("/trip")
+							.filters(gatewayFilterSpec ->
+									gatewayFilterSpec.setPath("/tripInfo"))
+							.uri("http://localhost:9292/"))
+					.route("payment", routeSpec -> routeSpec
+							.path("/pay")
+							.filters(gatewayFilterSpec ->
+									gatewayFilterSpec.setPath("/payment"))
+							.uri("http://localhost:9393/"))
+					.build();
+		}
+	}
