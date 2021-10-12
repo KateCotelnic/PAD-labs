99f4a9114012d3c2df1f97b828f530f1a64af6d1
Ecaterina Cotelnic [Intern] <Ecaterina.Cotelnic@endava.com>
2021-10-12 14:16:31 +0300

    update service trip

--- lab 1/tripService/src/main/java/com/example/tripService/config/WebSocketConfiguration.java
+++ lab 1/tripService/src/main/java/com/example/tripService/config/WebSocketConfiguration.java
@@ -1,6 +1,11 @@
 package com.example.tripService.config;
 
+import com.example.tripService.controller.TripController;
+import com.example.tripService.entity.Trip;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.var;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
@@ -26,4 +31,27 @@ public class WebSocketConfiguration {
     SimpleUrlHandlerMapping simpleUrlHandlerMapping(WebSocketHandler webSocketHandler){
         return new SimpleUrlHandlerMapping(Map.of("/currentTrip", webSocketHandler), 10);
     }
+
+//    private final ObjectMapper objectMapper;
+//
+//    @SneakyThrows
+//    private String from (Trip trip){
+//        return this.objectMapper.writeValueAsString(trip);
+//    }
+
+//    @Bean
+//    WebSocketHandler webSocketHandler1(){
+//        return  webSocketSession -> {
+////                var map = userFlux
+////                        .map(user -> from(user))
+////                        .map(webSocketSession::textMessage);
+//            return webSocketSession.send();
+//        };
+//    }
+//
+//    @Bean
+//    SimpleUrlHandlerMapping simpleUrlHandlerMapping1(WebSocketHandler webSocketHandler1){
+//        return new SimpleUrlHandlerMapping(Map.of("/myTrip", webSocketHandler1), 10);
+//    }
+//}
 }
--- lab 1/tripService/src/main/java/com/example/tripService/controller/TripController.java
+++ lab 1/tripService/src/main/java/com/example/tripService/controller/TripController.java
@@ -1,13 +1,12 @@
 package com.example.tripService.controller;
 
 import com.example.tripService.dto.TripDTO;
+import com.example.tripService.entity.Trip;
 import com.example.tripService.service.TripService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,8 +21,23 @@ public class TripController {
     )
     String post(@ModelAttribute TripDTO tripDTO){
         tripDTO.setTime(tripDTO.getLocation().length() + "");
-        tripDTO.setCost((double) tripDTO.getDestination().length());
+        tripDTO.setCost(tripDTO.getDestination().length() + "");
         tripService.addTrip(tripDTO);
         return "ok";
+    }
+
+    @GetMapping(
+            produces = MediaType.TEXT_EVENT_STREAM_VALUE,
+            value = "/myTrip/{id}"
+    )
+    TripDTO get(@PathVariable Long id){
+        Trip trip =  tripService.getByUserId(id);
+        TripDTO tripDTO = new TripDTO();
+        tripDTO.setTripType(trip.getTripType().toString());
+        tripDTO.setDestination(trip.getDestination());
+        tripDTO.setLocation(trip.getLocation());
+        tripDTO.setDriverId(trip.getDriverId().toString());
+        tripDTO.setUserId(trip.getUserId().toString());
+        return tripDTO;
     }
 }
